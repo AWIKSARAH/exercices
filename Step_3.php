74afa45c79dedc1c97b333b8d3a615ae504bc5ee
AWIKSARAH <awiksarah@gmail.com>
2023-02-15 22:34:00 +0200

    Step_3

---
+++ Step_3.php
@@ -0,0 +1,10 @@
+<?php
+$name= "Sarah";
+$email = "awiksarah@gmail.com";
+echo "My name is ".$name." and my email is ".$email;
+$height = 165;
+$width = 100;
+$area = $height * $width;
+print $area;
+
+?>
