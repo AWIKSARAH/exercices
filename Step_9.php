ca9e2d9c63adad5662eb0212ed9d8b2d9cc193f5
AWIKSARAH <awiksarah@gmail.com>
2023-02-16 18:43:23 +0200

    Step_9

---
+++ Step_9.php
@@ -0,0 +1,11 @@
+<?php
+// $grocery = array(
+//     'Eggs' => array('balade', 'mazere3'),
+//     'Milk' => array('Fresh', 'Taanayel'),
+//     'WaterPack' => array('Tanoureen', 'Reem')
+// );
+// var_dump($grocery);
+$name = "Sarah";
+var_dump($name);
+
+?>
