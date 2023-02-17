9cefd6d9ba915b2701abcd16cc8856fdeb8dcf7d
AWIKSARAH <awiksarah@gmail.com>
2023-02-17 21:34:35 +0200

    Basic 2 Step 9

--- basics2/Step_9.php
+++ basics2/Step_9.php
@@ -1,10 +1,19 @@
 <?php
 
 
+// function is_Power_of_two($n)
+// {
+//    return  $x = $n**(1/2);
+// }
+
+// print_r(is_Power_of_two(4));
+
+
 function is_Power_of_two($n)
 {
-   return  $x = $n**(1/2);
+echo ($n & ($n-1)) == 0 ? "{$n} is Power of 2" : "{$n} is not power of 2";
+
 }
 
-print_r(is_Power_of_two(4));
+is_Power_of_two(readline("Enter a Number"));
 ?>
